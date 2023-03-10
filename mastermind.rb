32c010874afa18d68a52f8e7621117eb157c283b
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-03-10 23:53:21 +0300

    Add function to display board

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,26 @@
 class Board
+    attr_accessor :code, :board, :guesses
+
     def initialize
         @code = Array.new(4) {0}
         @board = Array.new(10) {Array.new(4) {0}}            
         @guesses = Array.new(10) {Array.new(4)}
     end
+
+    def display_board(win)
+        if win
+            p code
+        else
+            p Array.new(4) {"Ø"}
+        end
+
+        board.each_with_index do |row, i|
+            print row
+            print "     "
+            p guesses[i]
+        end
+    end
 end
 
 board = Board.new
-puts board
+board.display_board(true)
